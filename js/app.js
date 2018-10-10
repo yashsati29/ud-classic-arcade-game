9e3012e0446fb9fc382e45ba07e2354594e9ae1d
DevS <yashsati29@gmail.com>
2018-10-11 05:05:01 +0530

    Updated Enemy.prototype.update function in app.js (JS)
    This commit includes the following changes inside the function: Speed Multiplier on Enemy has been added, enemy reappearing on canvas with different speed physx has been added when enemy reach to the end of the canvas also collision check has been added to check wheather the player has been collided by the enemy or not

--- js/app.js
+++ js/app.js
@@ -10,12 +10,24 @@ var Enemy = function(x, y, speed) {
 };
 
 // Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+	//Mutiplies speed by dt on x-axis
+	this.x += this.speed * dt;
+
+    //Once enemy reached at the end of the canvas they should reappear with different speeds.
+    if(this.x > 510) {
+    	this.x = -50;
+    	this.speed = 100 + Math.floor(Math.random() * 222);
+    };
+
+    //Checking for collision between player and the enemy
+    if (player.x < this.x + 80 && player.x + 80 > this.x &&
+    	player.y < this.y + 60 && 60 + player.y > this.y) {
+    	player.x = 202;
+    player.y = 405;
 };
+};
+
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
